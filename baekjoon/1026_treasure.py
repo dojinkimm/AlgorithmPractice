2df78fc84724155491361bc02702c35a1b9dc727
Henry <dojinkim119@gmail.com>
2019-09-17 22:10:28 -0400

    백준 1026 보물

--- baekjoon/1026_treasure.py
+++ baekjoon/1026_treasure.py
@@ -5,4 +5,5 @@ N = int(sys.stdin.readline())
 A = list(map(int, sys.stdin.readline().split()))
 B = list(map(int, sys.stdin.readline().split()))
 
-
+answer = sum([a*b for a, b in zip(sorted(A), sorted(B, reverse=True))])
+print(answer)
