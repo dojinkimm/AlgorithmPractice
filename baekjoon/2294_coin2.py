476dcb3a4a570f4ff11f8aac1ff73ce5bb7c5e88
Henry <dojinkim119@gmail.com>
2019-10-16 19:42:38 -0400

    백준 2294 동전 2

---
+++ baekjoon/2294_coin2.py
@@ -0,0 +1,14 @@
+# BOJ 2294 - 동전 2
+import sys
+r = sys.stdin.readline
+
+N, K = map(int, r().split())
+coins = sorted([int(r()) for _ in range(N)])
+
+arr = [10001] * (K+1)
+
+for i in range(N):
+    for j in range(coins[i], K+1):
+        arr[j] = min(arr[j], arr[j-coins[i]] + 1)
+
+print(arr[-1] if arr[-1] != 10001 else -1)
