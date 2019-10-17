51a2a2705dde5c9071826bd843d7fcb3aaff2f4b
Henry <dojinkim119@gmail.com>
2019-10-17 13:52:46 -0400

    fix error

--- baekjoon/2294_coin2.py
+++ baekjoon/2294_coin2.py
@@ -6,9 +6,12 @@ N, K = map(int, r().split())
 coins = sorted([int(r()) for _ in range(N)])
 
 arr = [10001] * (K+1)
+arr[0] = 0
 
 for i in range(N):
     for j in range(coins[i], K+1):
         arr[j] = min(arr[j], arr[j-coins[i]] + 1)
+        print(arr)
 
-print(arr[-1] if arr[-1] != 10001 else -1)
+arr[-1] = arr[-1] if arr[-1] != 10001 else -1
+print(arr[-1])
