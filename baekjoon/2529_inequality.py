bf7f7c60b2d471c93dd5b8565d7af6c49c6df435
dojinkimm <dojinkim119@gmail.com>
2020-01-10 15:39:31 +0900

    백준 2529 부등호 - 그리디

--- baekjoon/2529_inequality.py
+++ baekjoon/2529_inequality.py
@@ -1,5 +1,50 @@
 # BOJ 2529 - 부등호 - Greedy
 import sys
+from itertools import permutations
+
 r = sys.stdin.readline
 
 N = int(r())
+inequality = list(map(str, r().strip().split()))
+inc = [i for i in range(N+1)]
+dec = [i for i in range(9, 9-N-1, -1)]
+inequality.append(" ")
+
+result = 0
+flag = False
+for perm in permutations(dec):
+    for i in range(N):
+        if inequality[i] == '>' and perm[i] > perm[i+1]:
+            result += 1
+        elif inequality[i] == '<' and perm[i] < perm[i+1]:
+            result += 1
+        else:
+            break
+
+        if result == N:
+            flag = True
+            break
+    if flag:
+        print("".join(map(str,perm)))
+        break
+    result = 0
+
+result = 0
+flag = False
+for perm in permutations(inc):
+    for i in range(N):
+        if inequality[i] == '>' and perm[i] > perm[i+1]:
+            result += 1
+        elif inequality[i] == '<' and perm[i] < perm[i+1]:
+            result += 1
+        else:
+            break
+
+        if result == N:
+            flag = True
+            break
+    if flag:
+        print("".join(map(str, perm)))
+        break
+    result = 0
+
